e7165910bbecd5400c5c00a40bc6105b4558e134
AlexandraTritoiu <44137538+AlexandraTritoiu@users.noreply.github.com>
2018-10-14 16:30:29 +0300

    Landing Page

---
+++ Alexandra Tritoiu - Landing Page/slick/slick-initialize.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.slick-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 50000,
+        dots: true,
+        arrows: true,
+        // adaptiveHeight: true,
+    });
+});
